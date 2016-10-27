73dc141379e5ce1e47ddee03b5cd1dc56ded6af7
VDigitall <vitalldh@gmail.com>
2016-10-27 14:08:33 +0300

    Validation key decorator and tests

--- openprocurement/api/encryprion/tests/encryption.py
+++ openprocurement/api/encryprion/tests/encryption.py
@@ -29,18 +29,46 @@ class EncryptionTest(unittest.TestCase):
         self.assertEqual(response.status, '200 OK')
         self.assertEqual(decrypted_file, 'Very important information')
 
-        with self.assertRaises(webtest.AppError):
-            resp = self.app.post('/encrypt_file', collections.OrderedDict([('key', ''), ('file', webtest.forms.Upload('filename.txt', 'Very important information'))]))
-        with self.assertRaises(webtest.AppError):
-            resp = self.app.post('/encrypt_file', collections.OrderedDict([('key', 'a514cdc3c198421de6a746961d34f20147b7614c85a39297ffb07570b28hello'), ('file', webtest.forms.Upload('filename.txt', 'Very important information'))]))
-
-        with self.assertRaises(webtest.AppError):
-            response = self.app.post('/decrypt_file', collections.OrderedDict([('key', ''), ('file', webtest.forms.Upload('filename.txt', encrypted_file))]))
-        with self.assertRaises(webtest.AppError):
-            response = self.app.post('/decrypt_file', collections.OrderedDict([('key', 'a514cdc3c198421de6a746961d34f20147b7614c85a39297ffb07570b28hello'), ('file', webtest.forms.Upload('filename.txt', encrypted_file))]))
-        # self.assertEqual(response.status, '200 OK')
-        # self.assertNotEqual(encrypted_file, 'Very important information')
-
+        # Encrypt without file
+        response = self.app.post('/encrypt_file', collections.OrderedDict([('key', key)]), status=400)
+        self.assertEqual(response.status, '400 Bad Request')
+        self.assertEqual(response.json_body[u'errors'][0][u'description'], u'Missed file.')
+        # Decrypt decrypted_file
+        response = self.app.post('/decrypt_file', collections.OrderedDict([('key', key), ('file', webtest.forms.Upload('filename.txt', decrypted_file))]), status=500)
+        self.assertEqual(response.status, '500 Internal Server Error')
+        self.assertEqual(response.json_body[u'errors'][0][u'description'], u'Decryption failed. Ciphertext failed verification')
+        # Decrypt without file
+        response = self.app.post('/decrypt_file', collections.OrderedDict([('key', key)]), status=400)
+        self.assertEqual(response.status, '400 Bad Request')
+        self.assertEqual(response.json_body[u'errors'][0][u'description'], u'Missed encrypted file.')
+        # Encrypt with empty key
+        response = self.app.post('/encrypt_file', collections.OrderedDict([('key', ''), ('file', webtest.forms.Upload('filename.txt', 'Very important information'))]), status=422)
+        self.assertIn(response.status, '422 Unprocessable Entity')
+        self.assertEqual(response.json_body[u'errors'][0][u'description'], u'The key must be exactly 32 bytes long.')
+        # Encrypt with Invalid key
+        response = self.app.post('/encrypt_file', collections.OrderedDict([('key', 'a514cdc3c198421de6a746961d34f20147b7614c85a39297ffb07570b28hello'), ('file', webtest.forms.Upload('filename.txt', 'Very important information'))]), status=422)
+        self.assertIn(response.status, '422 Unprocessable Entity')
+        self.assertEqual(response.json_body[u'errors'][0][u'description'], u'Invalid key: Non-hexadecimal digit found.')
+        # Encrypt without key
+        response = self.app.post('/encrypt_file', collections.OrderedDict([('file', webtest.forms.Upload('filename.txt', 'Very important information'))]), status=422)
+        self.assertIn(response.status, '422 Unprocessable Entity')
+        self.assertEqual(response.json_body[u'errors'][0][u'description'], u'Key missed.')
+        # Decrypt with empty key
+        response = self.app.post('/decrypt_file', collections.OrderedDict([('key', ''), ('file', webtest.forms.Upload('filename.txt', encrypted_file))]), status=422)
+        self.assertIn(response.status, '422 Unprocessable Entity')
+        self.assertEqual(response.json_body[u'errors'][0][u'description'], u'The key must be exactly 32 bytes long.')
+        # Decrypt with invalid key
+        response = self.app.post('/decrypt_file', collections.OrderedDict([('key', 'a514cdc3c198421de6a746961d34f20147b7614c85a39297ffb07570b28hello'), ('file', webtest.forms.Upload('filename.txt', encrypted_file))]), status=422)
+        self.assertIn(response.status, '422 Unprocessable Entity')
+        self.assertEqual(response.json_body[u'errors'][0][u'description'], u'Invalid key: Non-hexadecimal digit found.')
+        # Decrypt without key
+        response = self.app.post('/decrypt_file', collections.OrderedDict([('file', webtest.forms.Upload('filename.txt', encrypted_file))]), status=422)
+        self.assertIn(response.status, '422 Unprocessable Entity')
+        self.assertEqual(response.json_body[u'errors'][0][u'description'], u'Key missed.')
+        # Decrypt with other key
+        response = self.app.post('/decrypt_file', collections.OrderedDict([('key', 'a7bfc49610fcd219021c86749f3ee09e1324d9c4de13f0d5f8cb569dd319e4e4'), ('file', webtest.forms.Upload('filename.txt', encrypted_file))]), status=500)
+        self.assertIn(response.status, '500 Internal Server Error')
+        self.assertEqual(response.json_body[u'errors'][0][u'description'], u'Decryption failed. Ciphertext failed verification')
 
 
 def suite():
--- openprocurement/api/encryprion/views.py
+++ openprocurement/api/encryprion/views.py
@@ -8,6 +8,7 @@ from .utils import generate_secret_key
 from .utils import encrypt_file
 from .utils import decrypt_file
 from .utils import validate_key
+from .utils import ValidationFailure, error
 
 @view_config(route_name='generate_key', renderer='json')
 def generate_key_view(request):
@@ -17,6 +18,12 @@ def generate_key_view(request):
 @view_config(route_name='encrypt_file', decorator=(validate_key,))
 def encrypt_file_view(request):
     key = request.POST.get('key').decode('hex')
+    encrypted_file = request.POST.get('file')
+    if encrypted_file == None:
+        error['message']['errors'][0]['description'] = 'Missed file.'
+        error['message']['errors'][0]['name'] = 'file'
+        error['code'] = 400
+        raise ValidationFailure(error)
     request.POST.get('file').file.seek(0)
     return encrypt_file(key, request.POST.get('file').file, nonce=request.POST.get('nonce'))
 
@@ -24,5 +31,11 @@ def encrypt_file_view(request):
 @view_config(route_name='decrypt_file', decorator=(validate_key,))
 def decrypt_file_view(request):
     key = request.POST.get('key').decode('hex')
+    encrypted_file = request.POST.get('file')
+    if encrypted_file == None:
+        error['message']['errors'][0]['description'] = 'Missed encrypted file.'
+        error['message']['errors'][0]['name'] = 'file'
+        error['code'] = 400
+        raise ValidationFailure(error)
     request.POST.get('file').file.seek(0)
     return decrypt_file(key, request.POST.get('file').file)
